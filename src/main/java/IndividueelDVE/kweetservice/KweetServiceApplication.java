89f4403a9311617c112b02a36dd776f6743ef0c4
DennisvEngelen <344709@student.fontys.nl>
2024-05-22 11:22:46 +0200

    initial commit

---
+++ src/main/java/IndividueelDVE/kweetservice/KweetServiceApplication.java
@@ -0,0 +1,13 @@
+package IndividueelDVE.kweetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KweetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KweetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/IndividueelDVE/kweetservice/KweetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package IndividueelDVE.kweetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KweetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
